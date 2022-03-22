080dd3128ed71fee42201ab6f7354a9a8b65dbc1
Hampus <hampus@adolfsson.email>
2022-03-22 19:25:42 +0100

    Add vertical sampler

--- win-rt-rgb/src/device/frame_sampler.rs
+++ win-rt-rgb/src/device/frame_sampler.rs
@@ -2,9 +2,10 @@ use super::RenderBuffer;
 
 /// A [FrameSampler] is responsible for sampling a captured [desktop_capture::Frame] and reducing it
 /// to a one-dimenstional [RenderBuffer] that can be processed further.
-pub trait FrameSampler {
+pub trait FrameSampler : Send {
     fn sample(&mut self, frame: &desktop_capture::Frame) -> RenderBuffer;
 }
+
 
 /// For an N-sized output buffer, divides each frame into N equally sized regions horizontally. Each region
 /// takes up the entire frame vertically. The output values are equal to the mean RGB values of each region.
@@ -43,6 +44,49 @@ impl FrameSampler for HorizontalFrameSampler {
                 self.buffer[i].green = (section_sums[i].green / 255) as f32 / pixels_in_section as f32;
                 self.buffer[i].blue  = (section_sums[i].blue / 255)  as f32 / pixels_in_section as f32;
             }
+        }
+        return self.buffer.clone();
+    }
+}
+
+
+/// For an N-sized output buffer, divides each frame into N equally sized regions vertically. Each region
+/// takes up the entire frame horizontally. The output values are equal to the mean RGB values of each region.
+pub struct VerticalFrameSampler {
+    buffer: RenderBuffer,
+}
+
+impl VerticalFrameSampler {
+    pub fn new(size: usize) -> Self {
+        VerticalFrameSampler{
+            buffer: vec![color::RgbF32::black(); size],
+        }
+    }
+}
+
+impl FrameSampler for VerticalFrameSampler {
+    fn sample(&mut self, frame: &desktop_capture::Frame) -> RenderBuffer {
+        // TODO: might want to avoid allocating vecs here
+        let section_height = frame.height as f64 / self.buffer.len() as f64;
+
+        for i in 0..self.buffer.len() {
+            let mut section_sum = color::Rgb{ red: 0u64, green: 0u64, blue: 0u64 };
+            let section_start = (i as f64 * section_height).ceil() as usize;
+            let section_end = ((i+1) as f64 * section_height).ceil() as usize;
+            for y in section_start..section_end {
+
+                for x in 0..frame.width {
+                    let val = frame.buffer[y * frame.width + x];
+                    section_sum.red   += val.red   as u64;
+                    section_sum.green += val.green as u64;
+                    section_sum.blue  += val.blue  as u64;
+                }
+
+            }
+            let pixels_in_section = frame.width * (section_end - section_start);
+            self.buffer[i].red   = (section_sum.red / 255)   as f32 / pixels_in_section as f32;
+            self.buffer[i].green = (section_sum.green / 255) as f32 / pixels_in_section as f32;
+            self.buffer[i].blue  = (section_sum.blue / 255)  as f32 / pixels_in_section as f32;
         }
         return self.buffer.clone();
     }
--- win-rt-rgb/src/device/mod.rs
+++ win-rt-rgb/src/device/mod.rs
@@ -8,7 +8,6 @@ pub use specification::*;
 pub use output::WledRenderOutput;
 
 use futures::stream::{ BoxStream, StreamExt };
-use frame_sampler::{ FrameSampler, HorizontalFrameSampler };
 
 type RenderBuffer = Vec::<color::RgbF32>;
 
@@ -21,8 +20,11 @@ pub struct RenderDevice<'a> {
 
 impl<'a> RenderDevice<'a> {
     pub fn new(spec: DeviceSpecification, frames: BoxStream<'a, desktop_capture::Frame>) -> Self {
-        let mut sampler = match spec.sampling_type {
+        use frame_sampler::{ FrameSampler, HorizontalFrameSampler, VerticalFrameSampler };
+
+        let mut sampler: Box<dyn FrameSampler> = match spec.sampling_type {
             SamplingType::Horizontal => Box::new(HorizontalFrameSampler::new(spec.output.size())),
+            SamplingType::Vertical => Box::new(VerticalFrameSampler::new(spec.output.size())),
             _ => panic!("Not implemented"),
         };
         if spec.adjustments.is_some() {
