7a60650d5a98c8cb8984051c06a867d60b09019e
Hampus <hampus@adolfsson.email>
2022-03-22 16:52:36 +0100

    Clean up horizontal sampler

--- win-rt-rgb/src/device/frame_sampler.rs
+++ win-rt-rgb/src/device/frame_sampler.rs
@@ -23,34 +23,26 @@ impl HorizontalFrameSampler {
 impl FrameSampler for HorizontalFrameSampler {
     fn sample(&mut self, frame: &desktop_capture::Frame) -> RenderBuffer {
         // TODO: might want to avoid allocating vecs here
-        let width = frame.width;
-        let mut column_sums = vec![0u32; 3*width];
-        {
-            for y in 0..frame.height {
-                for x in 0..frame.width {
-                    let val = frame.buffer[y * frame.width + x];
-                    column_sums[x]           += val.red as u32;
-                    column_sums[width + x]   += val.green as u32;
-                    column_sums[2*width + x] += val.blue as u32;
-                }
-            }
-        }
-
         let section_width = frame.width as f64 / self.buffer.len() as f64;
-        let mut section_sums = vec![0u32; 3 * self.buffer.len()];
-        for i in 0..self.buffer.len() {
-            let section_start = (i as f64 * section_width).ceil() as usize;
-            let section_end = ((i+1) as f64 * section_width).ceil() as usize;
-            for x in section_start..section_end {
-                section_sums[3*i]   += column_sums[x];
-                section_sums[3*i+1] += column_sums[width + x];
-                section_sums[3*i+2] += column_sums[2*width + x];
-            }
 
-            let pixels_in_section = frame.width * (section_end - section_start);
-            self.buffer[i].red   = section_sums[3*i]   as f32 / (pixels_in_section as f32 * 255.0);
-            self.buffer[i].green = section_sums[3*i+1] as f32 / (pixels_in_section as f32 * 255.0);
-            self.buffer[i].blue  = section_sums[3*i+2] as f32 / (pixels_in_section as f32 * 255.0);
+        let mut section_sums = vec![color::Rgb{ red: 0u64, green: 0u64, blue: 0u64 }; self.buffer.len()];
+        for y in 0..frame.height {
+            for i in 0..self.buffer.len() {
+                let section_start = (i as f64 * section_width).ceil() as usize;
+                let section_end = ((i+1) as f64 * section_width).ceil() as usize;
+
+                for x in section_start..section_end {
+                    let val = frame.buffer[y * frame.width + x];
+                    section_sums[i].red   += val.red   as u64;
+                    section_sums[i].green += val.green as u64;
+                    section_sums[i].blue  += val.blue  as u64;
+                }
+
+                let pixels_in_section = frame.height * (section_end - section_start);
+                self.buffer[i].red   = (section_sums[i].red / 255)   as f32 / pixels_in_section as f32;
+                self.buffer[i].green = (section_sums[i].green / 255) as f32 / pixels_in_section as f32;
+                self.buffer[i].blue  = (section_sums[i].blue / 255)  as f32 / pixels_in_section as f32;
+            }
         }
         return self.buffer.clone();
     }
